39b3d26f7f81a309c383747c4c12761d4ae4a172
Dongfang Zhao <dzhao@uw.edu>
2020-09-23 16:27:44 -0700

    now the coverted 28*28 image is generated at htd.py

--- examples/paper/pointcloud/htd.py
+++ examples/paper/pointcloud/htd.py
@@ -17,19 +17,28 @@ import torchvision
 import torchvision.datasets as datasets
 mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
 mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
-x, y = mnist_testset[5555]
+
+index = 3000
+x, y = mnist_trainset[index]
 x.show()
 # print(type(x))
 
-print(type(y))
-print("y =", y)
-print(len(mnist_testset))
+# print(type(y))
+# print("y =", y)
+# print(len(mnist_testset))
+
+for i in range(len(mnist_trainset)):
+    if 0 == i % 3000:
+        _, y_i =  mnist_trainset[i]
+        print("i = ", i, "y_i = ", y_i)
+print("So, trainset's labels are random.")
+
 
 for i in range(len(mnist_testset)):
     if 0 == i % 500:
-        print("i = ", i)
-
-exit(0)
+        _, y_i =  mnist_testset[i]
+        print("i = ", i, "y_i = ", y_i)
+print("So, testset's labels are random.")
 
 data_greylevel = np.rot90(np.rot90(np.rot90(np.array(x))))
 # print(type(data_greylevel))
@@ -116,33 +125,33 @@ plt.savefig(figfolder + 'uniform-H1.png')
 """
 
 # decrease H0
-print("Decrease H0")
-x = torch.autograd.Variable(torch.tensor(data).type(torch.float), requires_grad=True)
-print(x)
-print(type(x))
-layer = AlphaLayer(maxdim=0)
-f2 = BarcodePolyFeature(0,2,0)
-lr=1e-2
-optimizer = torch.optim.Adam([x], lr=lr)
-adjust = 2 #default adjuster
-for i in range(100):
-    optimizer.zero_grad()
-    loss = adjust * f2(layer(x))
-    loss.backward()
-    optimizer.step()
-    if loss.item() <= 2.0: # should have some eplison to avoid overfitting
-        break
-    if (0 == i % 5):
-        print ("[Iter %d] [top loss: %f]" % (i, loss.item()))
-
-y = np.rint(x.detach().numpy())
-print(y)
-print(y.shape)
-plt.figure(figsize=(5,5))
-plt.scatter(y[:,0], y[:,1])
-plt.axis('equal')
-plt.axis('off')
-plt.savefig(figfolder + 'adjust' + str(adjust) + '_uniform-H0.png')
+# print("Decrease H0")
+# x = torch.autograd.Variable(torch.tensor(data).type(torch.float), requires_grad=True)
+# print(x)
+# print(type(x))
+# layer = AlphaLayer(maxdim=0)
+# f2 = BarcodePolyFeature(0,2,0)
+# lr=1e-2
+# optimizer = torch.optim.Adam([x], lr=lr)
+# adjust = 2 #default adjuster
+# for i in range(100):
+#     optimizer.zero_grad()
+#     loss = adjust * f2(layer(x))
+#     loss.backward()
+#     optimizer.step()
+#     if loss.item() <= 2.0: # should have some eplison to avoid overfitting
+#         break
+#     if (0 == i % 5):
+#         print ("[Iter %d] [top loss: %f]" % (i, loss.item()))
+#
+# y = np.rint(x.detach().numpy())
+# print(y)
+# print(y.shape)
+# plt.figure(figsize=(5,5))
+# plt.scatter(y[:,0], y[:,1])
+# plt.axis('equal')
+# plt.axis('off')
+# plt.savefig(figfolder + 'adjust' + str(adjust) + '_uniform-H0.png')
 
 
 """
@@ -175,23 +184,32 @@ optimizer = torch.optim.Adam([x], lr=lr)
 layer = AlphaLayer(maxdim=1)
 f3 = BarcodePolyFeature(1,2,1)
 f4 = BarcodePolyFeature(0,2,0)
-for i in range(5000):
+for i in range(1000):
     optimizer.zero_grad()
     dgminfo = layer(x)
     # loss = -f3(dgminfo) + f4(dgminfo)
-    loss = -f3(dgminfo)*.1 + f4(dgminfo)*10
+    loss = -f3(dgminfo)*.5 + f4(dgminfo)*2
     loss.backward()
     optimizer.step()
     # print(type(loss.item()))
     if loss.item() <= 5.0: # should have some eplison to avoid overfitting
         break
-    if (i % 5 == 4):
+    if (i % 50 == 0):
         print ("[Iter %d] [top loss: %f]" % (i, loss.item()))
 
 y = x.detach().numpy().astype(int)
-plt.figure(figsize=(5,5))
-plt.scatter(y[:,0], y[:,1])
-plt.axis('equal')
-plt.axis('off')
-plt.savefig(figfolder + 'uniform+H1-H0.png')
 
+# plt.figure(figsize=(5,5))
+# plt.scatter(y[:,0], y[:,1])
+# plt.axis('equal')
+# plt.axis('off')
+# plt.savefig(figfolder + 'uniform+H1-H0.png')
+
+#convert the indices into a matrix of one's
+ma = np.zeros((28, 28))
+ma[tuple(y.T)] = 1
+# print("ma = ", ma)
+plt.figure(2)
+ma_rot90 = np.rot90(np.array(ma))
+plt.imshow(ma_rot90);
+plt.savefig(figfolder + 'uniform_matrix.png')
